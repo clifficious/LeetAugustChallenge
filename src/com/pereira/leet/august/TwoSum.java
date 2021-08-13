2f064e320a09d5015b8e5347aed011a17533445f
Cliff <cliff@MacBook-Pro-von-Cliff.local>
2021-08-14 00:11:46 +0200

    Initial commit

---
+++ src/com/pereira/leet/august/TwoSum.java
@@ -0,0 +1,27 @@
+package com.pereira.leet.august;
+
+import java.util.HashMap;
+
+/**
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ *
+ * <pre>
+ *     Input: nums = [2,7,11,15], target = 9
+ *     Output: [0,1]
+ *     Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+ * </pre>
+ */
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> values = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            if (values.keySet().contains(target - nums[i])){
+                return new int[]{values.get(target - nums[i]), i};
+            } else {
+                values.put(nums[i], i);
+            }
+        }
+        return new int[]{0,0};
+    }
+}
---
+++ test/com/pereira/leet/august/TwoSumTest.java
@@ -0,0 +1,43 @@
+package com.pereira.leet.august;
+
+import com.pereira.leet.august.TwoSum;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TwoSumTest {
+
+    @Test
+    @DisplayName("Test success")
+    void testTwoSum() {
+        TwoSum twoSum = new TwoSum();
+        int[] myArray = {2, 7, 11, 15};
+        int[] solution = twoSum.twoSum(myArray, 9);
+        assertNotNull(solution);
+        int[] expected = {0, 1};
+        assertArrayEquals(expected, solution);
+    }
+
+    @Test
+    @DisplayName("Edge case success")
+    void testTwoSumEdgeCase() {
+        TwoSum twoSum = new TwoSum();
+        int[] myArray = {2, 7, 11, 15};
+        int[] solution = twoSum.twoSum(myArray, 17);
+        assertNotNull(solution);
+        int[] expected = {0, 3};
+        assertArrayEquals(expected, solution);
+    }
+
+    @Test
+    @DisplayName("Nothing found")
+    void testTwoSumNothingFoundø() {
+        TwoSum twoSum = new TwoSum();
+        int[] myArray = {2, 7, 11, 15};
+        int[] solution = twoSum.twoSum(myArray, 19);
+        assertNotNull(solution);
+        int[] expected = {0, 0};
+        assertArrayEquals(expected, solution);
+    }
+}
